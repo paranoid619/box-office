5116b380d9c634bf8504ed2b5281ee9878aa5285
paranoid619 <souravbot123@gmail.com>
2021-07-24 14:52:23 +0530

    created pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import React from "react";
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Navs from './components/Navs';
+import Home from './pages/Home';
+import Starred from './pages/Starred';
 
 function App() {
   return (
-    <div >
-      hi
-      
+    <div>
+      <Navs />
+
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+
+        <Route exact path="/starred">
+          <Starred />
+        </Route>
+
+        <Route>
+          <div>Not found</div>
+        </Route>
+      </Switch>
     </div>
   );
 }
