19a4268b5fe2d6f80056dee5ed275becbd88f1d3
paranoid619 <souravbot123@gmail.com>
2021-07-25 14:30:18 +0530

    added search option

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,8 +8,12 @@ const Home = () => {
 
   const[results,setResults] = useState(null);
 
+  const[searchOption,setSearchOption] = useState('shows');
+
+  const isShowsSearch = searchOption==='shows';
+
   const onSearch =()=>{
-    apiGet(`/search/shows?q=${input}`).then(result=>{
+    apiGet(`/search/${searchOption}?q=${input}`).then(result=>{
       setResults(result);
     });
   
@@ -31,6 +35,13 @@ const Home = () => {
  };
 
 
+ const onRadioChange = (ev)=>{
+
+  setSearchOption(ev.target.value);
+
+ }
+
+
  const renderResults =() =>{
    if(results && results.length===0){
      return <div>
@@ -40,10 +51,11 @@ const Home = () => {
    
    if(results && results.length>0){
 
-    return <div> {results.map(item =>(
-    <div key ={item.show.id}>{item.show.name}</div>
-    ))}
-    </div>
+    return results[0].show
+    ? results.map(item => <div key ={item.show.id}>{item.show.name}</div>)
+         : results.map(item =>(
+        <div key ={item.person.id}>{item.person.name}</div>
+        ));
 
    }
 
@@ -56,7 +68,26 @@ const Home = () => {
 
 
   return <MainPageLayout>
-    <input type ="text" onChange ={onInputChange} onKeyDown={onKeyDown} value ={input}/>
+    <input type ="text"
+    placeholder ="Search for something"
+     onChange ={onInputChange} onKeyDown={onKeyDown} value ={input}/>
+
+
+    <div>
+
+   <label htmlFor="shows-search">
+     Shows
+     <input id="shows=search" type ="radio" value="shows" checked={isShowsSearch} onChange={ onRadioChange }/>
+   </label>
+
+
+   <label htmlFor="actors-search">
+     Actors
+     <input id="actors-search" type ="radio" value ="people" checked={!isShowsSearch} onChange={ onRadioChange }/>
+   </label>
+
+    </div>
+
     <button type ="button" onClick ={onSearch}>Search</button>
     { renderResults()}
   </MainPageLayout>;
