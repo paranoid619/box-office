5c6863fa267340fb5a967005407ea4b84d7e901f
paranoid619 <souravbot123@gmail.com>
2021-07-27 11:39:11 +0530

    using cutom hooks

--- src/components/show/ShowMainData.js
+++ src/components/show/ShowMainData.js
@@ -11,7 +11,7 @@ export const ShowMainData = ({ name, rating, summary, tags, image }) => {
           <Headline>
             <h1>{name}</h1>
             <div>
-              <Star />
+              <Star active/>
               <span>{rating.average || 'N/A'}</span>
             </div>
           </Headline>
